33209df6d01c95ef0141765984b46f9e2ee98643
Germey <cqc@cuiqingcai.com>
2018-10-06 21:24:15 +0800

    max epoch

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 tf.flags.DEFINE_string('data_dir', './fer2013/fer2013.csv', help='Data dir')
 tf.flags.DEFINE_float('learning_rate', 0.001, help='Learning Rate')
+tf.flags.DEFINE_integer('epochs', 1000, help='Max Epochs', allow_override=True)
 
 
 class Trainer(BaseTrainer):
